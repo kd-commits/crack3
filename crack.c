b9c040eb92bac7354350628f828de1c6556bea4c
Kevin Nguyen <knguyen38@student.sierracollege.edu>
2024-11-08 12:21:18 -0800

    cleanup of main | theia instead of vs

--- crack.c
+++ crack.c
@@ -11,55 +11,53 @@
 #define PASS_LEN 50     // Maximum length any password will be.
 #define HASH_LEN 33     // Length of hash plus one for null.
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fileutil.h"
+
 int main(int argc, char *argv[])
 {
-    if (argc < 3) 
+    if (argc < 3)
     {
         printf("Usage: %s hash_file dictionary_file\n", argv[0]);
         exit(1);
     }
 
-    // Read the hashes file into an array.
     int size;
     char **hashes = loadFileAA(argv[1], &size);
+    puts("File loaded");
     
-    // CHALLENGE1: Sort the hashes using qsort.
-    
-    // Open the password file for reading.
     FILE *d = fopen(argv[2], "r");
     if (!d)
     {
         puts("Cant open dictionary for reading");
         exit(1);
     }
-
+    
+    // malloc line
     char *line = malloc(PASS_LEN * sizeof(char));
     int count = 0;
 
-    // CHALLENGE1: Use binary search instead of linear search.
     while (fgets(line, PASS_LEN, d))
     {
-        // Trim
-        char *nl = strchr(line, '\n');
-        if (nl) *nl ='\0';
+		char *nl = strchr(line, '\n');
+		if (nl) *nl = '\0';
+		
+        char *target = md5(line, strlen(line));
+		char *found = substringSearchAA(target, hashes, size);
 
-        // Hash
-        char *target = md5(line, sizeof(line));
-
-        // Search
-        char *found = substringSearchAA(target, hashes, size);
-
-        if (strcmp(target,found) == 0)
+		if (found)
         {
-            printf("%s %s\n", found, line);
+			printf("%s %s\n", found, target);
             count++;
         }
-
-        free(target);
     }
-    
-    // TODO
+
     fclose(d);
-    printf("%d hashes cracked!\n", count);
+    // free line
+    free(line);
     freeAA(hashes, size);
+    printf("%d hashes cracked!\n", count);
 }
