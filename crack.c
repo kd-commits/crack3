6b56017b08c017f262a6dd1606bef84f1e8bed5d
Kevin Nguyen <knguyen38@student.sierracollege.edu>
2024-11-08 11:41:08 -0800

    Update crack.c | Add fileutil.c fileutil.

--- crack.c
+++ crack.c
@@ -11,7 +11,6 @@
 #define PASS_LEN 50     // Maximum length any password will be.
 #define HASH_LEN 33     // Length of hash plus one for null.
 
-
 int main(int argc, char *argv[])
 {
     if (argc < 3) 
@@ -20,29 +19,47 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // TODO: Read the hashes file into an array.
-    //   Use either a 2D array or an array of arrays.
-    //   Use the loadFile function from fileutil.c
-    //   Uncomment the appropriate statement.
+    // Read the hashes file into an array.
     int size;
-    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
-    //char **hashes = loadFile(argv[1], &size);
+    char **hashes = loadFileAA(argv[1], &size);
     
     // CHALLENGE1: Sort the hashes using qsort.
     
-    // TODO
     // Open the password file for reading.
+    FILE *d = fopen(argv[2], "r");
+    if (!d)
+    {
+        puts("Cant open dictionary for reading");
+        exit(1);
+    }
 
-    // TODO
-    // For each password, hash it, then use the array search
-    // function from fileutil.h to find the hash.
-    // If you find it, display the password and the hash.
-    // Keep track of how many hashes were found.
+    char *line = malloc(PASS_LEN * sizeof(char));
+    int count = 0;
+
     // CHALLENGE1: Use binary search instead of linear search.
+    while (fgets(line, PASS_LEN, d))
+    {
+        // Trim
+        char *nl = strchr(line, '\n');
+        if (nl) *nl ='\0';
 
+        // Hash
+        char *target = md5(line, sizeof(line));
+
+        // Search
+        char *found = substringSearchAA(target, hashes, size);
+
+        if (strcmp(target,found) == 0)
+        {
+            printf("%s %s\n", found, line);
+            count++;
+        }
+
+        free(target);
+    }
+    
     // TODO
-    // When done with the file:
-    //   Close the file
-    //   Display the number of hashes found.
-    //   Free up memory.
+    fclose(d);
+    printf("%d hashes cracked!\n", count);
+    freeAA(hashes, size);
 }
