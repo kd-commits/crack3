ae34a8fbbd93b878d28f72d0201b6407867ac4e5
Kevin Nguyen <knguyen38@student.sierracollege.edu>
2024-11-08 11:41:08 -0800

    # This is a combination of 9 commits. # This is the 1st commit message:
    First commit | Update crack.c | Add fileutil.c fileutil.
    
    # The commit message #2 will be skipped:
    
    # cleanup of main | theia instead of vs
    
    # The commit message #3 will be skipped:
    
    # Fixed realloc not expanding correctly
    
    # The commit message #4 will be skipped:
    
    # qsort challenge | +comments
    
    # The commit message #5 will be skipped:
    
    # tab to space
    
    # The commit message #6 will be skipped:
    
    # Remove functions for 2d (unused)
    
    # The commit message #7 will be skipped:
    
    # qsort challenge | +comments
    
    # The commit message #8 will be skipped:
    
    # qsort challenge | +comments
    # Fixed realloc error
    # Reset fileutil.c
    #
    # Remove functions for 2d
    #
    # qsort challenge | +comments
    #
    # tab to space
    #
    # Remove functions for 2d (unused)
    
    # The commit message #9 will be skipped:
    
    # qsort challenge | +comments

--- crack.c
+++ crack.c
@@ -11,38 +11,157 @@
 #define PASS_LEN 50     // Maximum length any password will be.
 #define HASH_LEN 33     // Length of hash plus one for null.
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fileutil.h"
+
+int alphabetic(const void *a, const void *b);
+int strsearch(const void *key, const void *elem);
+
+int alphabetic(const void *a, const void *b);
+int strsearch(const void *key, const void *elem);
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3) 
+    if (argc < 3)
     {
         printf("Usage: %s hash_file dictionary_file\n", argv[0]);
         exit(1);
     }
+<<<<<<< HEAD
+    
+=======
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 6f84749 (qsort challenge | +comments)
+>>>>>>> b576cd3 (qsort challenge | +comments)
     // TODO: Read the hashes file into an array.
     //   Use either a 2D array or an array of arrays.
     //   Use the loadFile function from fileutil.c
     //   Uncomment the appropriate statement.
+<<<<<<< HEAD
+    // Read the hashes file into an array.
+    
+=======
+<<<<<<< HEAD
+>>>>>>> b576cd3 (qsort challenge | +comments)
     int size;
-    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
-    //char **hashes = loadFile(argv[1], &size);
-    
+    char **hashes = loadFileAA(argv[1], &size);
+
     // CHALLENGE1: Sort the hashes using qsort.
-    
-    // TODO
-    // Open the password file for reading.
+    qsort(hashes, size, sizeof(char*), alphabetic);
 
     // TODO
+<<<<<<< HEAD
+=======
+=======
+    // Read the hashes file into an array.
+    
+    int size;
+    char **hashes = loadFileAA(argv[1], &size);
+
+    // CHALLENGE1: Sort the hashes using qsort.
+    qsort(hashes, size, sizeof(char*), alphabetic);
+
+    // TODO
+>>>>>>> b576cd3 (qsort challenge | +comments)
+    // Open the password file for reading.
+    FILE *d = fopen(argv[2], "r");
+    if (!d)
+    {
+        puts("Cant open dictionary for reading");
+        exit(1);
+    }
+
+
+    // TODO:
+<<<<<<< HEAD
+=======
+>>>>>>> 6f84749 (qsort challenge | +comments)
+>>>>>>> b576cd3 (qsort challenge | +comments)
     // For each password, hash it, then use the array search
     // function from fileutil.h to find the hash.
     // If you find it, display the password and the hash.
     // Keep track of how many hashes were found.
     // CHALLENGE1: Use binary search instead of linear search.
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
     // TODO
+=======
+>>>>>>> b576cd3 (qsort challenge | +comments)
+    char *line = malloc(PASS_LEN * sizeof(char));
+    int count = 0;
+    while (fgets(line, PASS_LEN, d))
+    {
+<<<<<<< HEAD
+        char *nl = strchr(line, '\n');
+        if (nl) *nl = '\0';
+                  
+=======
+		char *nl = strchr(line, '\n');
+		if (nl) *nl = '\0';
+		
+>>>>>>> b576cd3 (qsort challenge | +comments)
+        char *target = md5(line, strlen(line));
+        char *found = substringSearchAA(target, hashes, size); // linear search
+        if (found)
+        {
+<<<<<<< HEAD
+            printf("%s %s\n", line, found);
+=======
+			printf("%s %s\n", line, found);
+>>>>>>> b576cd3 (qsort challenge | +comments)
+            count++;
+        }
+    }
+
+<<<<<<< HEAD
+=======
+>>>>>>> 6f84749 (qsort challenge | +comments)
+>>>>>>> b576cd3 (qsort challenge | +comments)
     // When done with the file:
     //   Close the file
     //   Display the number of hashes found.
     //   Free up memory.
+<<<<<<< HEAD
+
+    fclose(d);
+    printf("%d hashes cracked!\n", count);
+    free(line);
+    freeAA(hashes, size);
 }
+=======
+<<<<<<< HEAD
+}
+=======
+
+    fclose(d);
+    printf("%d hashes cracked!\n", count);
+    free(line);
+    freeAA(hashes, size);
+}
+>>>>>>> b576cd3 (qsort challenge | +comments)
+
+int alphabetic(const void *a, const void *b)
+{
+    char ** aa = (char **)a;
+    char ** bb = (char **)b;
+
+    return strcmp(*aa,*bb);
+}
+
+int strsearch(const void *key, const void *elem)
+{
+    return strcmp((char**)key, (char**)elem);
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> 6f84749 (qsort challenge | +comments)
+>>>>>>> b576cd3 (qsort challenge | +comments)
---
+++ fileutil.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fileutil.h"
+
+// DIRECTIONS
+// Choose whether you are doing the 2D array or
+// the array of arrays.
+// For the 2D array,
+//    implement loadFile2D, substringSearch2D, and free2D.
+// For the array of arrays, 
+//    implement loadFileAA, substringSearchAA, and freeAA.
+
+// Global constants
+const int CAPACITY_START = 20;
+const int CAP_INCREMENT = 10;
+
+
+// Load the text file from the given filename.
+// Return a pointer to the array of strings.
+// Sets the value of size to be the number of valid
+// entries in the array (not the total array length).
+char ** loadFileAA(char *filename, int *size)
+{
+	FILE *in = fopen(filename, "r");
+	if (!in)
+	{
+	    perror("Can't open file");
+	    exit(1);
+	}
+	
+	// TODO
+	// Allocate memory for an array of strings (arr).
+	int CAPACITY = CAPACITY_START;
+	int SIZE = 0;
+	char line[1000];
+
+	// Array of arrays
+	char ** arr = malloc(CAPACITY * sizeof(char *));
+
+	// Read the file line by line.
+	while (fgets(line, sizeof(line), in))
+	{
+		// Trim newline.
+		char *nl = strchr(line, '\n');
+		if (nl) *nl = '\0';
+
+        // Expand array if necessary (realloc).
+        if (SIZE == CAPACITY)
+        {
+            arr = realloc(arr, (CAPACITY + CAP_INCREMENT) * sizeof(char *));
+            CAPACITY += CAP_INCREMENT;
+        }
+
+		// Allocate memory for the string (str).
+		char *str = malloc((strlen(line) + 1) * sizeof(char));
+		
+		// Copy each line into the string (use strcpy).
+		strcpy(str, line);
+
+		// Attach the string to the large array (assignment =).
+		arr[SIZE++] = str;
+	}
+    // Close the file.
+	fclose(in);
+
+	// The size should be the number of entries in the array.
+	*size = SIZE;
+
+	// Return pointer to the array of strings.
+	return arr;
+}
+
+char (*loadFile2D(char *filename, int *size))[COLS]
+{
+
+    // TODO
+
+    // Allocate memory for an 2D array, using COLS as the width.
+
+    // Read the file line by line into a buffer.
+
+        //   Trim newline.
+
+		//   Expand array if necessary (realloc).
+	
+		//   Allocate memory for the string (str).
+
+        //   Copy each line from the buffer into the array (use strcpy).
+
+    // The size should be the number of entries in the array.
+
+    // Return pointer to the array.
+
+}
+
+// Search the array for the target string.
+// Return the found string or NULL if not found.
+char * substringSearchAA(char *target, char ** lines, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (strcmp(lines[i], target) == 0) 
+			return target;
+	}
+	return NULL;
+}
+
+char * substringSearch2D(char *target, char (*lines)[COLS], int size)
+{
+    
+    return NULL;
+}
+
+// Free the memory used by the array
+void freeAA(char ** arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		free(arr[i]);
+	}
+	free(arr);
+}
+
+void free2D(char (*arr)[COLS])
+{
+	free(arr);
+}
