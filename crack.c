ac1b849f6bb7c7925857bd65ab825506f4aefb63
Kevin Nguyen <knguyen38@student.sierracollege.edu>
2024-11-08 20:00:11 -0800

    another squash commit

--- crack.c
+++ crack.c
@@ -20,12 +20,6 @@
 int alphabetic(const void *a, const void *b);
 int strsearch(const void *key, const void *elem);
 
-<<<<<<< HEAD
-int alphabetic(const void *a, const void *b);
-int strsearch(const void *key, const void *elem);
-
-=======
->>>>>>> aa91b6d (squash commits)
 int main(int argc, char *argv[])
 {
     if (argc < 3)
@@ -33,37 +27,10 @@ int main(int argc, char *argv[])
         printf("Usage: %s hash_file dictionary_file\n", argv[0]);
         exit(1);
     }
-<<<<<<< HEAD
-    
-=======
->>>>>>> aa91b6d (squash commits)
     // TODO: Read the hashes file into an array.
     //   Use either a 2D array or an array of arrays.
     //   Use the loadFile function from fileutil.c
     //   Uncomment the appropriate statement.
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // Read the hashes file into an array.
-    
-=======
-<<<<<<< HEAD
->>>>>>> b576cd3 (qsort challenge | +comments)
-=======
-    // Read the hashes file into an array.
->>>>>>> aa91b6d (squash commits)
-    int size;
-    char **hashes = loadFileAA(argv[1], &size);
-
-    // CHALLENGE1: Sort the hashes using qsort.
-    qsort(hashes, size, sizeof(char*), alphabetic);
-
-    // TODO
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> aa91b6d (squash commits)
     // Read the hashes file into an array.
     int size;
     char **hashes = loadFileAA(argv[1], &size);
@@ -72,10 +39,6 @@ int main(int argc, char *argv[])
     qsort(hashes, size, sizeof(char*), alphabetic);
 
     // TODO
-<<<<<<< HEAD
->>>>>>> b576cd3 (qsort challenge | +comments)
-=======
->>>>>>> aa91b6d (squash commits)
     // Open the password file for reading.
     FILE *d = fopen(argv[2], "r");
     if (!d)
@@ -86,12 +49,12 @@ int main(int argc, char *argv[])
 
 
     // TODO:
+>>>>>>> b576cd3 (qsort challenge | +comments)
     // For each password, hash it, then use the array search
     // function from fileutil.h to find the hash.
     // If you find it, display the password and the hash.
     // Keep track of how many hashes were found.
     // CHALLENGE1: Use binary search instead of linear search.
-<<<<<<< HEAD
 
 <<<<<<< HEAD
 =======
@@ -99,25 +62,18 @@ int main(int argc, char *argv[])
     // TODO
 =======
 >>>>>>> b576cd3 (qsort challenge | +comments)
-=======
->>>>>>> aa91b6d (squash commits)
     char *line = malloc(PASS_LEN * sizeof(char));
     int count = 0;
     while (fgets(line, PASS_LEN, d))
     {
-        char *nl = strchr(line, '\n');
-        if (nl) *nl = '\0';
-                  
+		char *nl = strchr(line, '\n');
+		if (nl) *nl = '\0';
+		
         char *target = md5(line, strlen(line));
         char *found = substringSearchAA(target, hashes, size); // linear search
         if (found)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
             printf("%s %s\n", line, found);
-=======
-			printf("%s %s\n", line, found);
->>>>>>> b576cd3 (qsort challenge | +comments)
             count++;
         }
     }
@@ -126,27 +82,17 @@ int main(int argc, char *argv[])
 =======
 >>>>>>> 6f84749 (qsort challenge | +comments)
 >>>>>>> b576cd3 (qsort challenge | +comments)
-=======
-            printf("%s %s\n", line, found);
-            count++;
-        }
-    }
->>>>>>> aa91b6d (squash commits)
     // When done with the file:
     //   Close the file
     //   Display the number of hashes found.
     //   Free up memory.
 <<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> aa91b6d (squash commits)
 
     fclose(d);
     printf("%d hashes cracked!\n", count);
     free(line);
     freeAA(hashes, size);
 }
-<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 }
@@ -158,8 +104,6 @@ int main(int argc, char *argv[])
     freeAA(hashes, size);
 }
 >>>>>>> b576cd3 (qsort challenge | +comments)
-=======
->>>>>>> aa91b6d (squash commits)
 
 int alphabetic(const void *a, const void *b)
 {
@@ -173,12 +117,8 @@ int strsearch(const void *key, const void *elem)
 {
     return strcmp((char**)key, (char**)elem);
 <<<<<<< HEAD
-<<<<<<< HEAD
 }
 =======
 }
 >>>>>>> 6f84749 (qsort challenge | +comments)
 >>>>>>> b576cd3 (qsort challenge | +comments)
-=======
-}
->>>>>>> aa91b6d (squash commits)
